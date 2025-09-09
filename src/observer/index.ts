37d716610c5eb6bcf2af016c8421a1b97672a8a4
Brandon Pzocik <pzocikbrandon@gmail.com>
2025-09-09 12:57:00 -0300

    cambios en comentarios

--- src/observer/index.ts
+++ src/observer/index.ts
@@ -8,16 +8,12 @@ export async function runObserver() {
   const ana = new Student("Ana");
   const luis = new Student("Luis");
 
-  // Suscribir alumnos
   prof.subscriber(ana);
   prof.subscriber(luis);
 
-  // Publicar primera tarea
-  prof.publish("TP1: Algoritmos — entrega viernes 18:00.");
+  prof.publish("TP1: DataScience — entrega 13:00.");
 
-  // Dar de baja a Luis
   prof.unsubscribe(luis);
 
-  // Publicar segunda tarea
-  prof.publish("TP2: Patrones — entrega martes 18:00.");
+  prof.publish("TP2: Proyecto — entrega 18:00.");
 }
